b25f89e209398d71e555563ff7b562a976218c90
lanbinleo <lanbinwolf@gmail.com>
2025-11-15 11:52:54 +0800

    feat(config): Add configuration management with load, save, and delete functions

---
+++ internal/config/config.go
@@ -0,0 +1,92 @@
+package config
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Config represents the user configuration
+type Config struct {
+	Username     string `json:"username"`
+	PasswordHash string `json:"password_hash"` // MD5 hash of password
+}
+
+// GetConfigPath returns the path to the config file
+func GetConfigPath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configDir := filepath.Join(homeDir, ".myxb")
+	if err := os.MkdirAll(configDir, 0700); err != nil {
+		return "", err
+	}
+
+	return filepath.Join(configDir, "config.json"), nil
+}
+
+// Load loads the configuration from disk
+func Load() (*Config, error) {
+	configPath, err := GetConfigPath()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil // No config file exists
+		}
+		return nil, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+// Save saves the configuration to disk
+func Save(config *Config) error {
+	configPath, err := GetConfigPath()
+	if err != nil {
+		return err
+	}
+
+	data, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(configPath, data, 0600)
+}
+
+// HashPassword returns MD5 hash of the password (first hash only, for storage)
+func HashPassword(password string) string {
+	hash := md5.Sum([]byte(password))
+	return strings.ToUpper(fmt.Sprintf("%X", hash))
+}
+
+// Delete removes the configuration file
+func Delete() error {
+	configPath, err := GetConfigPath()
+	if err != nil {
+		return err
+	}
+
+	if err := os.Remove(configPath); err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+
+	return nil
+}
